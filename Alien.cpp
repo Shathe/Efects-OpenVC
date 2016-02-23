d2ae59300b44ae16640762aad1631ff7f0fd841a
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2016-02-23 14:05:16 +0100

    Improvements

--- Alien.cpp
+++ Alien.cpp
@@ -7,14 +7,13 @@ int findBiggestContour(vector<vector<Point> >);
 /*
  * Apply some filters in order to  smoothen the image.
  */
-int applyFilters(Mat p1) {
-	Scalar white = Scalar(135, 135, 135);
+int applyFilters(Mat p1, int n) {
+	Scalar white = Scalar(100, 100, 100);
 	Scalar whitemax = Scalar(255, 255, 255);
-
 
 	// Filters: Blur and dilate
 	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
-	blur(p1, p1, Size(10,10));
+	blur(p1, p1, Size(2 * n, 2 * n));
 	inRange(p1, white, whitemax, p1);
 	dilate(p1, p1, element);
 	inRange(p1, white, whitemax, p1);
@@ -45,54 +44,64 @@ int main(int argc, char** argv) {
 	VideoCapture cap(0); // open the default camera
 	if (cap.isOpened()) {
 		// check if we succeeded
-		Mat p2;
+		Mat p2, p3, p4;
 
 		while (1) {
-			Mat frame;
+			Mat frame, frame2;
 			cap >> frame;
+			cap >> frame2;
 			if (frame.empty())
 				break; // end of video stream
 			Mat img_hist, hsv_image;
-		    Mat canny_output;
-		    vector<vector<Point> > contours;
-		    vector<Vec4i> hierarchy;
-			Scalar hsv_min2 = Scalar(0, 10, 20);
+			Mat canny_output;
+			vector<vector<Point> > contours;
+			vector<Vec4i> hierarchy;
+			Scalar hsv_min2 = Scalar(7, 10, 20);
 			Scalar hsv_max2 = Scalar(20, 150, 255);
 
 			/* Change the color space */
 			cvtColor(frame, img_hist, CV_BGR2HSV);
 			/*Apply a range, the skin range */
-			inRange(img_hist, hsv_min2, hsv_max2, p2);
+
+			inRange(img_hist, hsv_min2, hsv_max2, p4);
 			//Aply some filters
-			applyFilters(p2);
-			// Finde the contours
-			findContours(p2, contours, hierarchy, CV_RETR_TREE,
+			applyFilters(p4, 4);
+			p4.copyTo(p3);
+
+			/*Find the smallest contours */
+			Mat pequenos = Mat::zeros(p3.size(), CV_8UC1);
+			findContours(p3, contours, hierarchy, CV_RETR_TREE,
 					CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-			int s = findBiggestContour(contours);
 
-			//Draw the biggest contourn
-			Mat drawing = Mat::zeros(p2.size(), CV_8UC1);
-			drawContours(drawing, contours, s, Scalar(255), -1, 8, hierarchy, 0,
-					Point());
+			for (int i = 0; i < contours.size(); i++) {
 
-			//Color the skin
-			split(frame, channels);
-			split(p2, channels2);
+				int area = cv::contourArea(contours.at(i));
+				if (area < 1200)
+					drawContours(pequenos, contours, i, Scalar(255), -1, 8,
+							hierarchy, 0, Point());
+			}
+
+			split(frame2, channels);
+			/* Use the color Alien filter only if, p4, the pixels are likely to be skin and
+			 * if they are not small (pequenos)
+			 */
 			for (int i = 0; i < frame.rows; ++i)
 				for (int j = 0; j < frame.cols; ++j)
-					if (drawing.at<uchar>(i, j) >= 200) {
+					if (p4.at<uchar>(i, j) >= 254
+							&& pequenos.at<uchar>(i, j) < 200) {
 						channels[red].at<uchar>(i, j) = channels[red].at<uchar>(
-								i, j) / 2;
+								i, j) / 1.5;
 						channels[green].at<uchar>(i, j) = channels[green].at<
-								uchar>(i, j) / 2;
+								uchar>(i, j) / 1.5;
+						;
 						channels[blue].at<uchar>(i, j) =
-								channels[blue].at<uchar>(i, j) / 2;
+								channels[blue].at<uchar>(i, j) / 1.5;
 						channels[option].at<uchar>(i, j) = channels[option].at<
-								uchar>(i, j) + 90;
+								uchar>(i, j) + 75;
 					}
 
-			merge(channels, frame); //merge 3 channels including the modified 1st channel into one image
-			imshow("Alien!", frame);
+			merge(channels, frame2); //merge 3 channels including the modified 1st channel into one image
+			imshow("Alien!", frame2);
 
 			if (waitKey(1) == 27)
 				break; // stop capturing by pressing ESC
